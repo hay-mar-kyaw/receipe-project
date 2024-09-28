8d264fedb2693900b55e3dca1ee1e8e363e62bb5
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2024-09-28 16:11:35 +0700

    a receipe created

--- backend/app/Http/Controllers/ReceipeController.php
+++ backend/app/Http/Controllers/ReceipeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Receipe;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ReceipeController extends Controller
 {
@@ -38,10 +40,47 @@ class ReceipeController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * POST - api/receipes
+     * @param title, description, category_id, photo
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        try{
+
+            //validate
+            $validator=Validator::make(request()->all(),[
+                'title'=>'required',
+                'description'=>'required',
+                'category_id'=>['required',Rule::exists('categories','id')],
+                'image'=>'required'
+            ]);
+            // if validation fail
+
+            if($validator->fails()){
+                $flatteredErrors = collect($validator->errors())->flatMap(function ($e, $field){
+                    return [$field => $e[0]];
+                });
+                return response()->json([
+                    'errors'=>$flatteredErrors,
+                    'status'=>400
+                ], 400);
+            }
+
+            //if validation pass
+            $receipe=new Receipe();
+            $receipe->title=request('title');
+            $receipe->description=request('description');
+            $receipe->image=request('image');
+            $receipe->category_id=request('category_id');
+            $receipe->save();
+
+            return response()->json($receipe, 201);
+        }catch(Exception $e){
+            return response()->json([
+                'message'=>$e->getMessage(),
+                'status'=>500
+            ], 500);
+        }
     }
 
     /**
@@ -88,8 +127,25 @@ class ReceipeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Receipe $receipe)
+    public function destroy($id)
     {
-        //
+        try{
+            $receipe=Receipe::find($id);
+            if(!$receipe){
+                return response()->json([
+                    'message'=>'receipe not found',
+                    'status'=>404
+                ],404);
+            }
+            $receipe->delete();
+            return $receipe;
+
+
+            }catch(Exception $e){
+                return response()->json([
+                    'message'=>$e->getMessage(),
+                    'status'=>500
+                ], 500);
+            }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/receipes',[ReceipeController::class,'index']);
+Route::post('/receipes',[ReceipeController::class,'store']);
 Route::get('/receipes/{receipe}',[ReceipeController::class,'show']);
-Route::destroy('/receipes/{receipe}',[ReceipeController::class,'delete']);
+Route::delete('/receipes/{receipe}',[ReceipeController::class,'destroy']);
